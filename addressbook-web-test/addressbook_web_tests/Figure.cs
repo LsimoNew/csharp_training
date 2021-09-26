567ba362dcd332705a645a04d1dedf392585c012
LsimoNew <lsimo@mail.ru>
2021-09-26 21:37:23 +0300

    Добавил класс фигур

---
+++ addressbook-web-test/addressbook_web_tests/Figure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace addressbook_web_tests
+{
+    class Figure
+    {
+        private bool colored = false;
+        public bool Colored
+        {
+            get
+            {
+                return colored;
+            }
+            set
+            {
+                colored = value;
+            }
+        }
+    }
+}
---
+++ addressbook-web-test/addressbook_web_tests/Square.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace addressbook_web_tests
+{
+    class Square : Figure
+    {
+        private int size;
+        private bool colored = false;
+
+        public Square(int size)
+        {
+            this.size = size;
+        }
+
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+        }
+    }
+}
