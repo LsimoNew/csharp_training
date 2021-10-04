0419af05f72b8a4c7b9a9393d40fd8fb6951d6ba
LsimoNew <lsimo@mail.ru>
2021-10-04 12:15:27 +0300

    Helpers

---
+++ addressbook-web-test/addressbook_web_tests/appmanager/HelperBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    public class HelperBase
+    {
+        protected IWebDriver driver;
+
+        public HelperBase(IWebDriver driver) 
+        {
+            this.driver = driver;
+        }
+    }
+}
---
+++ addressbook-web-test/addressbook_web_tests/tests/TestBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    public class TestBase
+    {
+        protected ApplicationManager app;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            app = new ApplicationManager();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            app.Stop();
+        }
+    }
+}
