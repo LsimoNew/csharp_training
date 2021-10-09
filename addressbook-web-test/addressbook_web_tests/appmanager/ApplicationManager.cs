1b4e6df0ef1c1d9ab18b2bbe8c878c389e6217ef
LsimoNew <lsimo@mail.ru>
2021-10-09 20:02:52 +0300

    Destructor doesnt work

--- addressbook-web-test/addressbook_web_tests/appmanager/ApplicationManager.cs
+++ addressbook-web-test/addressbook_web_tests/appmanager/ApplicationManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using System.Threading;
+using System.Threading.Tasks;
 using OpenQA.Selenium.Support.UI;
 
 namespace WebAddressbookTests
@@ -16,8 +18,9 @@ namespace WebAddressbookTests
         protected NavigationHelper navigator;
         protected GroupHelper groupHelper;
 
+        private static ThreadLocal<ApplicationManager> app = new ThreadLocal<ApplicationManager>();
 
-        public ApplicationManager()
+        private ApplicationManager()
         {
             driver = new FirefoxDriver();
             baseURL = "http://localhost/addressbook";
@@ -27,15 +30,7 @@ namespace WebAddressbookTests
             groupHelper = new GroupHelper(this);
         }
 
-        public IWebDriver Driver 
-        {
-            get
-            {
-                return driver;
-            }
-        }
-
-        public void Stop()
+        ~ApplicationManager()
         {
             try
             {
@@ -44,6 +39,23 @@ namespace WebAddressbookTests
             catch (Exception)
             {
                 // Ignore errors if unable to close the browser
+            }
+        }
+
+        public static ApplicationManager GetInstance()
+        {
+            if (! app.IsValueCreated)
+            {
+                app.Value = new ApplicationManager();
+            }
+            return app.Value;
+        }
+
+        public IWebDriver Driver 
+        {
+            get
+            {
+                return driver;
             }
         }
 
---
+++ addressbook-web-test/addressbook_web_tests/tests/TestSuiteFixture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [SetUpFixture]
+    public class TestSuiteFixture
+    {
+        [OneTimeSetUp]
+        public void InitApplicationManager()
+        {
+            ApplicationManager app = ApplicationManager.GetInstance();
+            app.Navigator.GoToHomePage();
+            app.Auth.Login(new AccountData("admin", "secret"));
+        }
+    }
+}
